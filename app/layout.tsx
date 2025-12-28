0ed020340965edfeb834cd0f748b3fdad59681ff
Benjamin Wakida <benwaeldon@gmail.com>
2025-12-28 13:40:35 +0300

    Update favicon to Web3 MUK Club logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,21 +12,8 @@ export const metadata: Metadata = {
   description: "Official Code of Conduct for Web3 MUK Club at Makerere University",
   generator: "v0.app",
   icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
+    icon: "/images/web3-logo.jpg",
+    apple: "/images/web3-logo.jpg",
   },
 }
 
